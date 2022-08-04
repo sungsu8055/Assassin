5f102fc8c1e70036ffb16a516b054ce92e260ac9
c00oast <88479335+c00oast@users.noreply.github.com>
2022-08-04 14:49:48 +0900

    Revert "Merge branch 'main' into GeonWoo"
    This reverts commit 92c180d561e6d20263e11223d62b7845df9de4e3, reversing
    changes made to 9d6bcfeba7d5de55df8d56c97b17cdf6846514e2.

--- Source/Assassin/Private/Player/AssassinPlayer.cpp
+++ Source/Assassin/Private/Player/AssassinPlayer.cpp
@@ -6,7 +6,6 @@
 #include "Components/CapsuleComponent.h"
 #include <Camera/CameraComponent.h>
 #include <GameFramework/SpringArmComponent.h>
-#include <Components/ArrowComponent.h>
 
 // Sets default values
 AAssassinPlayer::AAssassinPlayer()
@@ -15,10 +14,11 @@ AAssassinPlayer::AAssassinPlayer()
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 90.0f);
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// 캐릭터 메쉬 설정
-	GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -87), FRotator(0, -90, 0));
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
@@ -30,13 +30,11 @@ AAssassinPlayer::AAssassinPlayer()
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
-	GetCharacterMovement()->MaxWalkSpeed = walkSpeed;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->SocketOffset = FVector(0, 0, 50);
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
@@ -44,12 +42,8 @@ AAssassinPlayer::AAssassinPlayer()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-	// 점프 가능 장애물 높이 컴포넌트
-	ableToJumpOverHeights = CreateDefaultSubobject<UArrowComponent>(TEXT("ableToJumpOverHeights"));
-	ableToJumpOverHeights->SetupAttachment(RootComponent);
-	// 애로우의 위치 높이값이 캐릭터가 넘을 수 있는 장애물 높이 값이 됨
-	ableToJumpOverHeights->SetRelativeLocation(FVector(0, 0, -40));
-
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
 
 // Called when the game starts or when spawned
@@ -73,16 +67,9 @@ void AAssassinPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 
 	// Set up gameplay key bindings
 	check(PlayerInputComponent);
-	// PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// 전력질주 모드 키 바인딩
-	PlayerInputComponent->BindAction("SprintMode", IE_Pressed, this, &AAssassinPlayer::OnActionSprintMode);
-	PlayerInputComponent->BindAction("SprintMode", IE_Released, this, &AAssassinPlayer::OnActionReleaseSprintMode);
-	// 내려가기 흐름 키 바인딩
-	PlayerInputComponent->BindAction("DownFlow", IE_Pressed, this, &AAssassinPlayer::OnActionDownFlow);
-
-	// 이동 키 바인딩 WASD
 	PlayerInputComponent->BindAxis("MoveForward", this, &AAssassinPlayer::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AAssassinPlayer::MoveRight);
 
@@ -90,7 +77,9 @@ void AAssassinPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
 	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AAssassinPlayer::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AAssassinPlayer::LookUpAtRate);
 }
 
 void AAssassinPlayer::MoveForward(float Value)
@@ -122,23 +111,15 @@ void AAssassinPlayer::MoveRight(float Value)
 	}
 }
 
-// 전력질주 모드 활성
-void AAssassinPlayer::OnActionSprintMode()
+void AAssassinPlayer::TurnAtRate(float Rate)
 {
-	// 전력질주 모드 속도로 변경
-	GetCharacterMovement()->MaxWalkSpeed = sprintSpeed;
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
 
-// 전력질주 모드 비활성
-void AAssassinPlayer::OnActionReleaseSprintMode()
+void AAssassinPlayer::LookUpAtRate(float Rate)
 {
-	// 일반이동 모드 속도로 변경
-	GetCharacterMovement()->MaxWalkSpeed = walkSpeed;
-}
-
-// 전력질주 모드 - 내려가기 흐름
-void AAssassinPlayer::OnActionDownFlow()
-{
-
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
